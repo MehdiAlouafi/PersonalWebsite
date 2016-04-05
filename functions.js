f9939b33a37b4b99ebb4e9008ef28fc237464f63
Mehdi Al Ouafi <alouafi.mehdi@gmail.com>
2016-04-05 04:54:14 +0200

    Resizing the side bar menu and adding some effects to the current selected item

--- functions.js
+++ functions.js
@@ -3,4 +3,11 @@ $(function(){
    $(this).toggleClass('open');
    $('.menu').toggleClass('is_open');
  });
+ $('.menu_items').click(function(){
+   $('.menu_items').not(this).removeClass('is_current');
+   $(this).toggleClass('is_current');
+  //  $('.menu_items').removeClass('is_current');
+
+ })
+
 });
