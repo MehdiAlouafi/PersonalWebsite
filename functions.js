3f3f585af04667e4b2a745ea115257bae229c16e
Mehdi Al Ouafi <alouafi.mehdi@gmail.com>
2016-04-12 19:18:27 +0200

    Starting a v2 of the portfolio

--- functions.js
+++ functions.js
@@ -1,38 +1,12 @@
 
 $(function(){
-   $('.hamburger-menu').click(function(){
-     $('.overlay').toggleClass('is_active');
-     $(this).toggleClass('open');
-     $('.menu').toggleClass('is_open');
-   });
 
-   $('.menu_items').click(function(){
-     $('.menu_items').not(this).removeClass('is_current');
-     $(this).toggleClass('is_current');
-    //  $('.menu_items').removeClass('is_current');
-
-  });
 
 //J'avais un problème par rapport à la détection d'un élément une fois entré dans le viewport
 // en utilisant le plugin jquery visible, j'ai remarqué que la vérification ne se lancé qu'une fois
 // ainsi avec window.onscroll, à chaque fois que l'utilisateur scroll je vérifie si tel élément est dans le viewport
 //Si oui je lance une action (ici un console.log)
 
-$(window).scroll(function(){
-  var wScroll = $(this).scrollTop();
-  $('.kicker').css({
-    'transform':' translate(0,'+wScroll / 30 +'%)'
-  });
-  $('.first-planet').css({
-    'transform': 'translate(0,'+ wScroll/10 + '%)'
-  });
-  $('.second-planet').css({
-    'transform': 'translate(0,'+ wScroll/3 + '%)'
-  });
-  $('.third-planet').css({
-    'transform': 'translate(0,'+ wScroll/40 + '%)'
-  });
 
-  });
 
 });
