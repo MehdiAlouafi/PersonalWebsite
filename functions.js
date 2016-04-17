7f98b1fe975683379d85d9df3c5eaa9d990751c0
Mehdi Al Ouafi <alouafi.mehdi@gmail.com>
2016-04-17 04:06:26 +0200

    Responsive always ! Going to merge to gh-pages to test it on iphone

--- functions.js
+++ functions.js
@@ -6,10 +6,11 @@ $(function(){
 // en utilisant le plugin jquery visible, j'ai remarqué que la vérification ne se lancé qu'une fois
 // ainsi avec window.onscroll, à chaque fois que l'utilisateur scroll je vérifie si tel élément est dans le viewport
 //Si oui je lance une action (ici un console.log)
-
 $('.menu_item').click(function(){
   $(this).addClass('current_item').siblings().removeClass('current_item');
 });
-
+$('.nav-responsiv').click(function(){
+  $('.nav-responsiv-menu').toggleClass('is_open')
+});
 
 });
